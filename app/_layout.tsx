9d4dd370fbdecde1886027dce35a7aec63332774
Willane Paiva <willanepaiva@hotmail.com>
2025-09-20 14:45:45 -0300

    add screen title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="index" options={{ title: "Desculpator 3000" }}></Stack.Screen>
+  </Stack>;
 }
